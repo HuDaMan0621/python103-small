be1129d63f21b478e2166eb398c89313f37fe4a3
Ying Xuan Hu <hudaman@Yings-MacBook-Pro.local>
2020-04-02 11:09:52 -0400

    exercise 7 added

---
+++ looping_from_1to10.py
@@ -0,0 +1,7 @@
+#7. Looping from 1 to 10
+#Using a while loop, print out the numbers between 1 and 10 inclusive, one on a line. Example output:
+count = 0
+while count <= 10:
+    print(count)
+    count = count + 1
+    
